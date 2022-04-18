549722f65c48cb40302ad87f9180e4f694248361
Yuri Vitor <yuri.lins@gft.com>
2022-04-18 15:38:04 -0300

    ex3

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import Exercicio1 from './ex1'
-import Exercicio2 from './ex2'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Exercicio3 from './ex3';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <Exercicio1 /> */}
-    <Exercicio2 />
+    {/* <Exercicio2 /> */}
+    <Exercicio3 />
   </React.StrictMode>
-)
+);

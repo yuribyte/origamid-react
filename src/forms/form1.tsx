a553fc899af02ca8b19684351d4873e8dfc15d3a
Yuri Vitor <yuri.lins25@gmail.com>
2022-04-27 20:06:21 -0300

    improve form with object

--- src/forms/form1.tsx
+++ src/forms/form1.tsx
@@ -1,29 +1,41 @@
 import { FormEventHandler, useState } from 'react';
 
 const Form1 = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
   function handleSubmit(event: Event | FormEventHandler | any) {
     (event as Event).preventDefault();
   }
 
+  function handleChange(event: React.FormEvent<HTMLInputElement>) {
+    const { id, value } = event.currentTarget;
+
+    setForm({ ...form, [id]: value });
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="name">Name</label>
-      <input
-        id="name"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+      <input id="name" type="text" value={form.name} onChange={handleChange} />
       <label htmlFor="email">Email</label>
       <input
         id="email"
         type="email"
-        value={email}
+        value={form.email}
+        onChange={handleChange}
         required
-        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        value={form.password}
+        onChange={handleChange}
+        required
       />
       <button>Enviar</button>
     </form>
